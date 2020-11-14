30d57ffef5746784982e1f29c373f145c31c23e3
Josh <zylo.codes@gmail.com>
2020-11-14 12:37:43 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var planet = {
+//   name: 'Earth',
+//   age: "4.543 billion years",
+//   moons: 1,
+//   isPopulated: true,
+//   population: "7.594 billion",
+//   neighboringPlanets: ["Mars", "Venus"],
+//   logFacts: function () {
+//     console.log("This planet's name is " + this.name)
+//     console.log("This planet's age is " + this.age)
+//   }
+// }
+
+// planet.logFacts()
